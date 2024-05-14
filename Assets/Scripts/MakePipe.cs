33c5252c648fbd6078e4da03e5943baaded49eac
ccjngwn <ahaha620@gmail.com>
2024-05-14 16:32:31 +0900

    Feat: 랜덤 장애물

--- Assets/Scripts/MakePipe.cs
+++ Assets/Scripts/MakePipe.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MakePipe : MonoBehaviour
 {
     [SerializeField] private GameObject pipe;
+    [SerializeField] private float timeDiff; // 파이프 나오는 시간
+    private float timer = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,13 @@ public class MakePipe : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer > timeDiff)
+        {
+            GameObject newpipe = Instantiate(pipe);
+            newpipe.transform.position = new Vector3(10, Random.Range(-6.4f, -2.3f), 0); // 새로운 파이프 생성 위치
+            timer = 0;
+            Destroy(newpipe, 11.0f);
+        }
     }
 }
