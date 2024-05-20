73bbcb058dcf278e0d6116b709b281b8de0819d6
ccjngwn <ahaha620@gmail.com>
2024-05-20 22:36:08 +0900

    Fix: 코인 먹으면 사라지게

--- Assets/Scripts/CoinSpawnManager.cs
+++ Assets/Scripts/CoinSpawnManager.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class CoinSpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject coin;
-    private float timeDiff = Random.Range(6.0f, 9.0f); // 아이템 나오는 랜덤한 시간
+    private float timeDiff; // 아이템 나오는 랜덤한 시간
     private float timer = 0; // 현재 시간
     
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeDiff = Random.Range(6.0f, 9.0f);
     }
 
     // Update is called once per frame
@@ -22,15 +22,9 @@ public class CoinSpawnManager : MonoBehaviour
         if (timer >= timeDiff)
         {
             GameObject newCoin = Instantiate(coin);
-            newCoin.transform.position = new Vector3(10, Random.Range(-3.15f, 0), 0); // 랜덤 코인 생성 위치
+            newCoin.transform.position = new Vector3(10, Random.Range(-3.3f, 0), 0); // 랜덤 코인 생성 위치
             timeDiff = Random.Range(6.0f, 9.0f);
             timer = 0;
-            Destroy(newCoin, 11.0f);
         }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        
     }
 }
