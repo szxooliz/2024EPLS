2416d6dc8a5c27205a11a8375f7204ab78052866
ccjngwn <ahaha620@gmail.com>
2024-05-20 21:27:33 +0900

    Feat: 코인 랜덤하게 생성

--- Assets/Scripts/CoinSpawnManager.cs
+++ Assets/Scripts/CoinSpawnManager.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class CoinSpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject coin;
-    [SerializeField] private float timeDiff;
-    private float timer = 0;
+    private float timeDiff = Random.Range(6.0f, 9.0f); // 아이템 나오는 랜덤한 시간
+    private float timer = 0; // 현재 시간
     
     
     // Start is called before the first frame update
@@ -19,5 +19,18 @@ public class CoinSpawnManager : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
+        if (timer >= timeDiff)
+        {
+            GameObject newCoin = Instantiate(coin);
+            newCoin.transform.position = new Vector3(10, Random.Range(-3.15f, 0), 0); // 랜덤 코인 생성 위치
+            timeDiff = Random.Range(6.0f, 9.0f);
+            timer = 0;
+            Destroy(newCoin, 11.0f);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
     }
 }
