a2795b8c528aaf1a7f24261c2317a0bb6dab9874
jokebear <julie0805114@gmail.com>
2024-08-01 16:25:00 +0900

    Reapply "커밋 : 패턴 구현 (꺄아아아!)"
    This reverts commit 627ed40063c63e0758406f017d9c62ff67245727.

--- Assets/Scripts/Spawn/SpawnPoint.cs
+++ Assets/Scripts/Spawn/SpawnPoint.cs
@@ -5,40 +5,27 @@ using UnityEngine;
 public class SpawnPoint : MonoBehaviour
 {
     public GameObject[] prefabToSpawn;
-    public float repeatInterval;
     private GameObject newPattern;
-    private float speed;
-    [SerializeField] private float acceleration = 10f;
-    private float timer;
-    private float spawnTimer;
+    public float spawnThresholdX = -30f;
+    public float destroyThresholdX = -70f;
 
     public void Start()
     {
-        speed = BackGroundLoop.speed;
-        acceleration = BackGroundLoop.acceleration;
-        repeatInterval = 48 / speed;
+        SpawnObject();
     }
 
     private void Update()
     {
-        speed = BackGroundLoop.speed;
-        acceleration = BackGroundLoop.acceleration;
-        DestroyPattern();
-
-        timer += Time.deltaTime;
-        spawnTimer += Time.deltaTime;
-
-        if (spawnTimer >= repeatInterval)
+        if (newPattern != null)
         {
-            SpawnObject();
-            spawnTimer = 0f;  // Reset the spawn timer
-        }
+            Destroy(newPattern, 15.0f);
 
-        if (timer > acceleration)
-        {
-            speed += 2f;
-            repeatInterval = 48 / speed;
-            timer -= acceleration;
+            // Check if the current pattern's x position is less than or equal to the spawn threshold
+            if (newPattern.transform.position.x <= spawnThresholdX)
+            {
+                SpawnObject();
+                
+            }
         }
     }
 
@@ -51,10 +38,5 @@ public class SpawnPoint : MonoBehaviour
             return newPattern;
         }
         return null;
-    }
-
-    public void DestroyPattern()
-    {
-        Destroy(newPattern, 11.0f);
     }
 }
