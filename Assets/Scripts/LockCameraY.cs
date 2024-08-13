9eb85c2634bf0364b9bfd178f25d81c47664c236
jokebear <julie0805114@gmail.com>
2024-08-13 23:14:07 +0900

    Feat : 시네머신 y축 고정 시킴

---
+++ Assets/Scripts/LockCameraY.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using Cinemachine;
+
+/// <summary>
+/// An add-on module for Cinemachine Virtual Camera that locks the camera's Z co-ordinate
+/// </summary>
+[ExecuteInEditMode]
+[SaveDuringPlay]
+public class LockCameraY : CinemachineExtension
+{
+    [Tooltip("Lock the camera's Y position to this value")]
+    public float m_YPosition = 0;
+
+    protected override void PostPipelineStageCallback(
+        CinemachineVirtualCameraBase vcam,
+        CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+    {
+        if (stage == CinemachineCore.Stage.Body)
+        {
+            var pos = state.RawPosition;
+            pos.y = m_YPosition;
+            state.RawPosition = pos;
+        }
+    }
+}
